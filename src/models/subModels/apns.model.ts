e21f482b350f0d10f9b4f6b2afee3f301e887a53
alexdm0 <alexdemulder@gmail.com>
2022-12-09 13:30:13 +0100

    fixed app model definition

--- src/models/subModels/apns.model.ts
+++ src/models/subModels/apns.model.ts
@@ -1,14 +1,13 @@
 import {Entity, model, property} from '@loopback/repository';
-import {Notifications_apnsKey} from "./apns-key.model";
 
 @model()
 export class Notifications_apns extends Entity {
   @property()
-  keyToken: Notifications_apnsKey;
+  keyToken: string;
 
   @property()
-  keyId: Notifications_apnsKey;
+  keyId: string;
 
   @property()
-  teamId: Notifications_apnsKey;
+  teamId: string;
 }
